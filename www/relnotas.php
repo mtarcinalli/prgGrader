c95dc97291c35425e85c6b79ec540722365f9466
Mateus Machado <mateusmachado@usp.br>
2022-08-02 08:47:44 -0300

    removing warnings

--- www/relnotas.php
+++ www/relnotas.php
@@ -1,13 +1,4 @@
-<?php
-require_once 'header.php';
-
-#$db = new SQLite3('../db/pgrader.db');
-if (! $db) {
-	echo "não abriu bd";
-}
-
-?>
-
+<?php require_once 'header.php'; ?>
 <form action="relnotas.php" method="post" role="form" class="form-horizontal">
 <div class="form-group">
     <div class="col-sm-10">
@@ -22,8 +13,9 @@ $tbl = $db->prepare($cmd);
 $tbl->execute();
 while ($row = $tbl->fetch()){
     echo "<option value='$row[codturma]' ";
-    if ($row['codturma'] == $_REQUEST['codturma'])
-        echo " selected";
+    if (isset($_REQUEST['codturma']))
+        if ($row['codturma'] == $_REQUEST['codturma'])
+            echo " selected";
     echo ">$row[curso] - $row[turma]</option>";
 }
 ?>
@@ -33,12 +25,9 @@ while ($row = $tbl->fetch()){
     				<button type="submit" class="btn btn-primary">Filtrar</button>
 			</div>
 </div>
-
 </form>
-
 <?php
-
-if ($_REQUEST["codturma"]) {
+if (isset($_REQUEST["codturma"])) {
     # tarefas
     $cmd = "select tt.codtarefaturma, t.sigla ".
             "FROM tarefaturma tt ".
@@ -57,7 +46,6 @@ if ($_REQUEST["codturma"]) {
             $avaliacoes[] = $row["codtarefaturma"];
         }
     }
-
     # alunos
     $cmd = "SELECT a.codaluno, a.nome, a.email, codturma ".
                 "FROM aluno a ".
@@ -69,7 +57,6 @@ if ($_REQUEST["codturma"]) {
     while ($row = $tblAlunos->fetch(PDO::FETCH_ASSOC)) {
         $alunos[] = $row;
     }
-
     # notas
     $cmd = "SELECT tta.codtarefaturma, codaluno, notafinal, codturma ".
                 "FROM tarefaturmaaluno tta ".
@@ -83,29 +70,31 @@ if ($_REQUEST["codturma"]) {
     while ($row = $tblAlunos->fetch(PDO::FETCH_ASSOC)) {
         $notas[$row["codaluno"]][$row["codtarefaturma"]] = $row["notafinal"];
     }
-
     # exibição
     echo "<table class=\"table\">".
             "<tr>".
-            "<th>Aluno</th>";
-            #"<th>E-mail</th>";
+            "<th>Aluno</th>".
+            "<th>E-mail</th>";
     foreach ($tarefas as $tarefa) {
         echo "<th>$tarefa[sigla]</th>";
     }
     echo "<th>FINAL</th></tr>";
-
     foreach ($alunos as $aluno) {
         echo "<tr>";
-        echo "<td>$aluno[nome] ($aluno[email] - $aluno[codturma] )</td>";
-        #echo "<td>$aluno[email]</td>";
+        echo "<td>$aluno[nome]</td>";
+        echo "<td>$aluno[email]</td>";
         $notasAv = 0;
         $notasTrab = 0;
         foreach ($tarefas as $tarefa) {
-            echo "<td class=\"text-center\">" . $notas[$aluno["codaluno"]][$tarefa["codtarefaturma"]] . "</td>\n";
-            if (in_array($tarefa["codtarefaturma"], $avaliacoes)) {
-                $notasAv += $notas[$aluno["codaluno"]][$tarefa["codtarefaturma"]];
+            if (array_key_exists($aluno["codaluno"], $notas)) {
+                echo "<td class=\"text-center\">" . $notas[$aluno["codaluno"]][$tarefa["codtarefaturma"]] . "</td>\n";
+                if (in_array($tarefa["codtarefaturma"], $avaliacoes)) {
+                    $notasAv += $notas[$aluno["codaluno"]][$tarefa["codtarefaturma"]];
+                } else {
+                    $notasTrab += $notas[$aluno["codaluno"]][$tarefa["codtarefaturma"]];
+                }
             } else {
-                $notasTrab += $notas[$aluno["codaluno"]][$tarefa["codtarefaturma"]];
+                echo "<td class=\"text-center\"></td>\n";
             }
         }
         $notaFinal = ceil(  ($notasAv / 2 * 0.7) + ($notasTrab / 7 * 0.3));
@@ -114,23 +103,10 @@ if ($_REQUEST["codturma"]) {
         }
         echo "<td class=\"text-center\">$notaFinal</td>";
         echo "</tr>";
-    
     }
     echo "</table>";
-
-
-
 }
-
 ?>
-
-
-
-
-
-
-
-
     </div>
 </body>
 </html>
