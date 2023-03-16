d712b1bb9e2ecc6d386124ef50517ec63dcdbc2c
Mateus Machado <mateusmachado@usp.br>
2023-03-16 11:46:44 -0300

    curso class tests

--- tests/prgGraderTests.php
+++ tests/prgGraderTests.php
@@ -1,8 +1,14 @@
-<?php declare(strict_types=1);
+<?php 
+#declare(strict_types=1);
 require_once '../src/modules/obj2db/src/obj2db.php';
 require_once '../src/conectdb.php';
+
+require_once '../src/classes/acessocurso.php';
 require_once '../src/classes/curso.php';
+require_once '../src/classes/tipousuario.php';
 require_once '../src/classes/turma.php';
+require_once '../src/classes/usuario.php';
+
 
 use PHPUnit\Framework\TestCase;
 
@@ -13,6 +19,10 @@ final class prgGraderTests extends TestCase
         parent::setUp();
         global $db;
 		$cmd = "DELETE FROM turma";
+		$tbl = $db->prepare($cmd);
+		$tbl->execute();
+
+		$cmd = "DELETE FROM acessocurso";
 		$tbl = $db->prepare($cmd);
 		$tbl->execute();
 
@@ -29,10 +39,14 @@ final class prgGraderTests extends TestCase
     public function testCurso(): void
     {
         global $db;
+        $codProprietario = 1;
+
+        # inserção
         $curso1 = new Curso();
         $curso1->setDescricao("Curso 01");
         $curso1->setSigla("CT 01");
         $curso1->setObservacao("Obs");
+        $curso1->proprietario($codProprietario);
         $curso1->salvar();
         $curso2 = new Curso();
         $curso2->carregar($curso1->getCodCurso());
@@ -49,6 +63,16 @@ final class prgGraderTests extends TestCase
         $this->assertSame($curso1->getSigla(), $row['sigla']);
         $this->assertSame($curso1->getObservacao(), $row['observacao']);
 
+		$cmd = "SELECT * FROM acessocurso WHERE codcurso = :codcurso";
+		$tbl = $db->prepare($cmd);
+        $tbl->bindValue(':codcurso', $curso1->getCodCurso(), PDO::PARAM_INT);
+		$tbl->execute();
+		$row = $tbl->fetch();
+        $this->assertSame($curso1->getCodCurso(), $row['codcurso']);
+        $this->assertSame($codProprietario, $row['codusuario']);
+        $this->assertSame($curso1->listaAcessos()[0], 1);
+
+        # alteração
         $curso1->setDescricao("Curso 001");
         $curso1->setSigla("CT 001");
         $curso1->setObservacao("Observação");
@@ -62,16 +86,28 @@ final class prgGraderTests extends TestCase
         $this->assertSame($curso1->getSigla(), $row['sigla']);
         $this->assertSame($curso1->getObservacao(), $row['observacao']);
 
+        # exclusão após inserção de turma
         $turma = new Turma();
         $turma->setCurso($curso1);
         $turma->setDescricao("Turma 01");
         $turma->setSigla("T01");
         $turma->setObservacao("Obs 01");
         $turma->salvar();
+        try {
+            $curso1->excluir();
+        } catch (Exception $e) {
+        }
+		$cmd = "SELECT * FROM curso WHERE codcurso = :codcurso";
+		$tbl = $db->prepare($cmd);
+        $tbl->bindValue(':codcurso', $curso2->getCodCurso(), PDO::PARAM_INT);
+		$tbl->execute();
+		$row = $tbl->fetch();
+        $this->assertSame($e->getCode(), 23503);
+        $this->assertSame($curso1->getSigla(), $row['sigla']);
+        $this->assertSame($curso1->getObservacao(), $row['observacao']);
 
-
-
-
+        # eclusão após exclusão de turma
+        $turma->excluir();
         $curso1->excluir();
 		$cmd = "SELECT * FROM curso WHERE codcurso = :codcurso";
 		$tbl = $db->prepare($cmd);
@@ -79,8 +115,8 @@ final class prgGraderTests extends TestCase
 		$tbl->execute();
 		$row = $tbl->fetch();
         $this->assertSame($row, false);
-        #$this->assertSame($curso1->getSigla(), $row['sigla']);
-        #$this->assertSame($curso1->getObservacao(), $row['observacao']);
+
+
     }
 
     /**
@@ -89,10 +125,13 @@ final class prgGraderTests extends TestCase
     public function testTurma(): void
     {
         global $db;
+        $codProprietario = 1;
+
         $curso = new Curso();
         $curso->setDescricao("Curso 01");
         $curso->setSigla("CT 01");
         $curso->setObservacao("Obs");
+        $curso->proprietario($codProprietario);
         $curso->salvar();
         $codcurso = $curso->getCodCurso();
 
