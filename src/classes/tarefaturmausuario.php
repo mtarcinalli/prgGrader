f0380e267214a8842798df2591f7ddac15bce73a
Mateus Machado <mateusmachado@usp.br>
2023-03-17 16:02:32 -0300

    corrigir method implementation

--- src/classes/tarefaturmausuario.php
+++ src/classes/tarefaturmausuario.php
@@ -1,5 +1,5 @@
 <?php
-class TarefaTurmaUsuario extends obj2db {			
+class TarefaTurmaUsuario extends obj2db {
 	private $codTarefaTurmaUsuario;
 	private $TarefaTurma;
 	private $Usuario;
@@ -18,15 +18,15 @@ class TarefaTurmaUsuario extends obj2db {
 
 	function getCodTarefaTurmaUsuario() {
 		return $this->codTarefaTurmaUsuario;
-	}			
+	}
 
 	function setCodTarefaTurmaUsuario($valor) {
 		$this->codTarefaTurmaUsuario = $valor;
-	}			
+	}
 
 	function getTarefaTurma() {
 		return $this->TarefaTurma;
-	}			
+	}
 
 	function setTarefaTurma($valor) {
 		if (is_numeric($valor)) {
@@ -38,11 +38,11 @@ class TarefaTurmaUsuario extends obj2db {
 		} else {
 			$this->TarefaTurma = $valor;
 		}
-	}			
+	}
 
 	function getUsuario() {
 		return $this->Usuario;
-	}			
+	}
 
 	function setUsuario($valor) {
 		if (is_numeric($valor)) {
@@ -54,54 +54,138 @@ class TarefaTurmaUsuario extends obj2db {
 		} else {
 			$this->Usuario = $valor;
 		}
-	}			
+	}
 
 	function getDataEntrega() {
 		return $this->dataEntrega;
-	}			
+	}
 
 	function setDataEntrega($valor) {
 		$this->dataEntrega = $valor;
-	}			
+	}
 
 	function getEntregas() {
 		return $this->entregas;
-	}			
+	}
 
 	function setEntregas($valor) {
 		$this->entregas = $valor;
-	}			
+	}
 
 	function getResultados() {
 		return $this->resultados;
-	}			
+	}
 
 	function setResultados($valor) {
 		$this->resultados = $valor;
-	}			
+	}
 
 	function getNota() {
 		return $this->nota;
-	}			
+	}
 
 	function setNota($valor) {
 		$this->nota = $valor;
-	}			
+	}
 
 	function getNotafinal() {
 		return $this->notafinal;
-	}			
+	}
 
 	function setNotafinal($valor) {
 		$this->notafinal = $valor;
-	}			
+	}
 
 	function getObservacao() {
 		return $this->observacao;
-	}			
+	}
 
 	function setObservacao($valor) {
 		$this->observacao = $valor;
-	}			
+	}
+
+	function carregar($cod, $codUsuario=0) {
+		if (! $codUsuario) {
+			return parent::carregar($cod);
+		} else {
+			$this->clear();
+			$this->select("codtarefaturmausuario");
+			$this->where("codtarefaturma = $cod AND codusuario = $codUsuario");
+			$this->listar();
+			$row = $this->retornar();
+			if ($row) {
+				parent::carregar($row["codtarefaturmausuario"]);
+				return true;
+			}
+			return false;
+		}
+	}
+
+	function salvar($forcarInsert=false) {
+		$inserir = ! $this->codTarefaTurmaUsuario;
+		parent::salvar($forcarInsert);
+		if ($inserir) {
+			$codcurso = $this->getTarefaTurma()->getTurma()->getCurso()->getCodCurso();
+			$codturma = $this->getTarefaTurma()->getTurma()->getCodTurma();
+			$codtarefaturma = $this->getTarefaTurma()->getCodTarefaTurma();
+			$codtarefaturmausuario = $this->getCodTarefaTurmaUsuario();
+			$cmd = "mkdir -p ../uploads/CURSO$codcurso/TURMA$codturma/TTURMA$codtarefaturma/TTALUNO$codtarefaturmausuario";
+			$output = shell_exec($cmd);
+		}
+
+	}
+
+	function corrigir($tarefaTurma, $usuario, $arquivo) {
+		if (! $this->carregar($tarefaTurma->getCodTarefaTurma(), $usuario->getCodUsuario())) {
+			$this->setTarefaTurma($tarefaTurma);
+			$this->setUsuario($usuario);
+			$this->setEntregas(0);
+			$this->setNotafinal(null);
+			$this->salvar();
+		}
+
+
+		$codtarefaturma = $tarefaTurma->getCodTarefaTurma();
+		$codturma = $tarefaTurma->getTurma()->getCodTurma();
+		$codcurso = $tarefaTurma->getTurma()->getCurso()->getCodCurso();
+		$codtarefaturmausuario = $this->getCodTarefaTurmaUsuario();
+		$codtarefa = $tarefaTurma->getTarefa()->getCodTarefa();;
+		$codcorretor = $tarefaTurma->getTarefa()->getCorretor()->getCodCorretor();
+
+		$uploaddir = "../uploads/CURSO$codcurso/TURMA$codturma/TTURMA$codtarefaturma/TTALUNO$codtarefaturmausuario/";
+		$uploadfile = $uploaddir . "arquivo.zip";
+
+		$files = glob($uploaddir . "*");
+		foreach($files as $file){
+			if(is_file($file)) {
+				unlink($file);
+			}
+		}
+
+		if (substr($arquivo, -8) == "temp.zip") {
+			rename($arquivo, $uploadfile);
+		} else {
+			if (!move_uploaded_file($arquivo, $uploadfile)) {
+				throw new Exception("Erro ao enviar arquivo!");
+			}
+		}
+
+
+		$this->setDataEntrega(date("Y-m-d"));
+		$this->entregas += 1;
+		if (! $this->notafinal) {
+			$this->notafinal = null;
+		}
+
+		$cmd = "cd $uploaddir && ls && echo '---' && " .
+					"ls && " .
+					"unzip -j arquivo.zip && " .
+					"cp -a ../../../../TAREFAS/T" . $codtarefa .  "/solution/* . && " .
+					"cp -a ../../../../CORRETORES/CORRETOR" . $codcorretor .  "/corretor/* . && " .
+					"bash ./grader.sh";
+		$output = trim(shell_exec($cmd));
+		$this->nota = intval(trim(substr($output, strrpos($output, "\n"), -1)));
+		$this->salvar();
+	}
 
 }
